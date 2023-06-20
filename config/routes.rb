39afc7040dc268c6e68683f95d4ebf9d27306b04
Vladimir Ivanov <51359590+IVladimirA@users.noreply.github.com>
2023-06-21 02:23:15 +0300

    fix: remove user new route (#11)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # root "articles#index"
-  resources(:users, only: %i[new create])
+  resources(:users, only: %i[create])
   post('logout', to: 'users#logout')
   get('auth', to: 'users#login')
   post('refresh', to: 'users#refresh')
