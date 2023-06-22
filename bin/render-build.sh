cebb67f6994b655baa824ff7a60c247896e33623
IVladimirA <ivanov.vladimir102002@gmail.com>
2023-06-22 22:27:49 +0300

    fix: migration script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,4 +8,7 @@ echo AAAAAAAAAAAAAAAAAA
 echo BBBBBBBBBBBB
 echo CCCCCCCCCCCCCC
 
+bundle exec rake db:migrate
 bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rake db:migrate
